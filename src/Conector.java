4b8d1cd2609b2f76b2e88c2aa81498932844121b
Daniel Lopez <tauron32@gmail.com>
2012-06-13 11:34:52 +0200

    Parseando constraints

--- src/Conector.java
+++ src/Conector.java
@@ -6,6 +6,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
@@ -88,15 +89,56 @@ public class Conector {
         		for (int j = 0; j < nodeListColumn.getLength(); j++) {
         			Node columnNode = nodeListColumn.item(j);
         			if (columnNode.getNodeType() == Node.ELEMENT_NODE) {
-        				Element el = (Element) columnNode;
-        				System.out.println("\tColumn name: " + getTagValue("name",el));
+        				Element columnElement = (Element) columnNode;
+        				System.out.println("\tColumn name: " + getTagValue("name",columnElement));
+        				System.out.println("\tColumn attribute: " + columnElement.getAttribute("idcol"));
+        				System.out.println("\tColumn data type: " + getTagValue("data_type",columnElement));
         			}
         		}
-        		
+        	}
+        	System.out.println("Segunda pasada para los constraints");
+        	for (int i = 0; i < tableNodeList.getLength(); i++) {
+        		// Recorremos la lista de nodos "table" que hayamos encontrado
+        		Node tableNode = tableNodeList.item(i);
+        		// Creamos un nuevo nodo con el elemento a obtener datos
+        		if (tableNode.getNodeType() == Node.ELEMENT_NODE) {
+        			// Confirmamos que el nodo es un elemento simple
+        			Element nameElement = (Element) tableNode;
+        			// Transformamos el nodo en elementos
+        			System.out.println("Table name: " + getTagValue("name",nameElement));
+        		}
+        		XPath xpath = XPathFactory.newInstance().newXPath();
+        		NodeList constraintList = (NodeList) xpath.evaluate("constraint",tableNode,XPathConstants.NODESET);
+        		parseConstraints(constraintList,c);
         	}
         } catch (Exception e) {
         	e.printStackTrace();	
         }
+    }
+    
+
+    private static void parseConstraints(NodeList nodeList, Conector c) throws XPathExpressionException {
+    	XPath xpath = XPathFactory.newInstance().newXPath();
+    	
+    	for (int j = 0; j < nodeList.getLength(); j++) {
+			Node constraintNode = nodeList.item(j);
+			if (constraintNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element constraintElement = (Element) constraintNode;
+				System.out.println("\tConstraint name: " + constraintElement.getAttribute("idcons"));
+				String constraintType = getTagValue("type",constraintElement);
+				System.out.println("\t\tConstraint type: " + constraintType);
+				if (constraintType.equals("U")) {
+					// Unique constraint
+					NodeList uniqueList = (NodeList) xpath.evaluate("constraint/unique_members",constraintNode,XPathConstants.NODESET);
+				} else if (constraintType.equals("P")) {
+					// Primary key constraint
+				} else if (constraintType.equals("F")) {
+					// Foreign key constraint
+				} else if (constraintType.equals("C")) {
+					// Check constraint
+				}
+			}
+		}
     }
     
     private static String getTagValue(String stringTag, Element element) {
